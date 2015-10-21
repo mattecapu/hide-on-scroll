1aef686b01bdf789a8509b21a95a67d6b983da73
Matteo Capucci <mattecapu@live.it>
2015-10-21 14:37:15 +0200

    first commit

---
+++ navbar.js
@@ -0,0 +1,65 @@
+/*! CREDIT: https://medium.com/@mariusc23/hide-header-on-scroll-down-show-on-scroll-up-67bbaae9a78c#.v77rs2jif */
+
+import $ from 'jquery';
+
+const cssPrefix = ($el, rule, value, prValue) => {
+	['-webkit-', '-moz-', '-ms-', '-o-', ''].forEach((prefix) => {
+		$el.css(prefix + rule, (prValue ? prefix : '') + value);
+	});
+}
+
+export default function navbar({navbarSelector, hidingClass, pollInterval = 200}) {
+	$(() => {
+		const delta = 5;
+		const $navbar = $(navbarSelector);
+
+		let didScroll;
+		let lastScrollTop = 0;
+
+		let navbarHeight = $navbar.outerHeight();
+
+		if (!hidingClass) {
+			cssPrefix($navbar, 'transition-property', 'transform', true);
+		}
+
+		$(window).scroll(() => didScroll = true);
+
+		function hasScrolled() {
+			const st = $(window).scrollTop();
+
+			// Make sure they scroll more than delta
+			if (Math.abs(lastScrollTop - st) <= delta) {
+				return;
+			}
+
+			// If they scrolled down and are past the navbar, add class .nav-up.
+			// This is necessary so you never see what is "behind" the navbar.
+			if (st > lastScrollTop && st > navbarHeight){
+				// Scroll Down
+				if (hidingClass) {
+					$navbar.addClass(hidingClass);
+				} else {
+					cssPrefix($navbar, 'transform', 'translateY(-100%)');
+				}
+			} else {
+				// Scroll Up
+				if (st + $(window).height() < $(document).height()) {
+					if (hidingClass) {
+						$navbar.removeClass(hidingClass);
+					} else {
+						cssPrefix($navbar, 'transform', 'translateY(0)');
+					}
+				}
+			}
+
+			lastScrollTop = st;
+		}
+
+		setInterval(() => {
+			if (didScroll) {
+				hasScrolled();
+				didScroll = false;
+			}
+		}, pollInterval);
+	});
+}
